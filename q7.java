05609e22effc5e816b597864a65fc57de8e1fc58
Shivangibose <53455642+Shivangibose@users.noreply.github.com>
2019-08-04 22:03:38 +0530

    Add files via upload

---
+++ q7.java
@@ -0,0 +1,27 @@
+public class q7
+{
+    public static void main(String args[])
+    {
+        int t=0,f=0;
+        for(int x=1;x>0;x++)
+        {
+            for(int i=1;i<=x;i++)
+            {
+            if(x%i==0)
+            {
+                f++;
+            }
+        }
+        if(f==2)
+        {
+            t++;
+        }
+        f=0;
+        if(t==10001)
+        {
+            System.out.println(x);
+        break;
+    }
+}
+}
+}
