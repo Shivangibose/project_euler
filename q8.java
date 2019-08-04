109134a20f084fca0f342c44720ee3295123aed7
Shivangibose <53455642+Shivangibose@users.noreply.github.com>
2019-08-04 22:04:41 +0530

    Add files via upload

---
+++ q8.java
@@ -0,0 +1,23 @@
+public class q8
+{
+    public static void main(String args[])
+    {
+        int s=0,f=0;
+        for(int x=1;x<=2000000;x++)
+        {
+            for(int i=1;i<=x;i++)
+            {
+                if(x%i==0)
+                {
+                    f++;
+                }
+            }
+            if(f==2)
+            {
+                s=s+x;
+            }
+            f=0;
+        }
+        System.out.println(s);
+    }
+}
