216d68d832d8665e9d6497e768bf6ed622287ee7
Shivangibose <53455642+Shivangibose@users.noreply.github.com>
2019-08-07 15:15:30 +0530

    Create q9.java

---
+++ q9.java
@@ -0,0 +1,25 @@
+public class q9 //there exists exactly one pythagorean triplet for which a+b+c=1000.find product abc.
+{
+    public static void main(String args[])
+   {
+    int a=0,b=0,c=0,s=1000,ctr=0;
+    for( a=1;a<s/3;a++)
+    {
+        for( b=a;b<s/2;b++)
+        {
+            c=s-a-b;
+            if(a*a+b*b==c*c)
+            {
+                ctr++;
+                break;
+            }
+        }
+        if(ctr>0)
+        {
+          System.out.println(a*b*c);
+        
+        }
+    }
+      
+   }
+}
