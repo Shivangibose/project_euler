0d373658fe2645b33d403313b885646059621bd5
Shivangibose <53455642+Shivangibose@users.noreply.github.com>
2019-08-07 13:52:49 +0530

    Add files via upload

---
+++ q4.java
@@ -0,0 +1,27 @@
+public class q4
+{ 
+    public static void main(String args[])
+    {
+        int p,d=0,n,r=0,max=0;
+        for(int a=100;a<=999;a++)
+        {
+            for(int b=100;b<=999;b++)
+            {
+                p=a*b;n=p;
+                while(p>0)
+                {
+                    d=p%10;
+                    r=(r*10)+d;
+                    p=p/10;
+                }
+                if(n==r&&n>max)
+                {
+                    max=r;
+                    r=0;
+                }
+            }
+        }
+        System.out.println(max);
+    }
+}
+                
