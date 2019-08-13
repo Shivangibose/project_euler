e9717c5b55a1bdeab1395dbbb2c81d30901bb8f4
Shivangibose <53455642+Shivangibose@users.noreply.github.com>
2019-08-13 12:31:58 +0530

    Update q10.java

--- q10.java
+++ q10.java
@@ -2,24 +2,21 @@ public class q10 // to find the index of the first term in the fibonacci sequenc
 {
     public static void main(String args[])
     {
-        int a=1,b=1,c=0,p=2,ctr=0,x,d=0;
-        while(c>0)
+        int a=1,b=1,c=0,p=2;
+        while(c>=0)
         {
             c=a+b;
             x=c;
             p++;
-            while(c>0)
-            {
-                d=x%10;
-                ctr++;
-                x=x/10;
-            }
-            if(ctr==1000)
-            {
-                System.out.println(p);
-                break;
-            }
-            ctr=0;
-        }
+       
+            String x=c;
+            int t=x.length();
+             if(t==1000)
+             {
+               System.out.println(p);
+              }
+             a=b;
+             b=c;
+          }
     }
 }
