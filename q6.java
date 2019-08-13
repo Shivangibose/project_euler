36b9e339217b3c333f733c5cf598dc3cea8a4072
Shivangibose <53455642+Shivangibose@users.noreply.github.com>
2019-08-13 06:23:51 +0530

    Update q6.java

--- q6.java
+++ q6.java
@@ -1,4 +1,4 @@
-public class q6
+public class q6 // to find the difference between sum of square of first 100 natural number and square of the sum.
 {
     public static void main(String args[])
     {
