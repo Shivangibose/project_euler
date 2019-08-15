6ad70c49a596195c30756bec57578e6ccfc835f0
Shivangibose <53455642+Shivangibose@users.noreply.github.com>
2019-08-15 13:44:48 +1000

    Update q6.java

--- q6.java
+++ q6.java
@@ -4,8 +4,8 @@ public class q6 // to find the difference between sum of square of first 100 nat
     {
         int s1=0,s2=0;
         s1=(100*(100+1)*(2*100+1))/6;
-        s2=Math.pow(100*(100+1)/2,2);
+        s2=(100*101)/2;
          
-        System.out.println(s2-s1);
+        System.out.println((s2*s2)-s1);
     }
 }
