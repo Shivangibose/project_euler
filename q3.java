79f9439a6abcc21ecb03ee828e1e26e64461e7d9
Shivangibose <53455642+Shivangibose@users.noreply.github.com>
2019-08-18 07:35:40 +0530

    Update q3.java

--- q3.java
+++ q3.java
@@ -1,27 +1,21 @@
 public class q3 //to find the largest prime factor of 600851475143
+
 {
     public static void main(String args[])
     {
-        long x=600851475143l;
-        long max;
-        double i=Math.sqrt(x);
-        for(int t=3;t<i;t=t+2)
+        long x=600851475143L;
+        long max;       
+        for(long t=3;t<x;t=t+2)
         {
-            while(x%t==0)
+            while(x%t==0 && t<x)
             {
                 x=x/t;
             }
         }
-        if(x>2)
-        {
-            System.out.println(x);
-            max=x;
-            System.out.println("largest="+max);
-        }
+        max=x;
+        System.out.println("largest prime factor="+max);
     }
 }
-        
-        
         
         
         
