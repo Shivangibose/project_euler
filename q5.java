8de84727946aa482df4eea0e6efc16ac94b690df
Shivangibose <53455642+Shivangibose@users.noreply.github.com>
2019-08-20 06:56:59 +0530

    Update q5.java

--- q5.java
+++ q5.java
@@ -1,40 +1,34 @@
-public class q5 // to find the smallest positive number that is evenly divisible by all of the numbers from 1 to 20.
-
+public class q5//to find the smallest positive number divisible by all numbers from 1 to 20
 {
-
     public static void main(String args[])
-
     {
-
-        int f=0;
-
-        for(int x=1;x>0;x++)
-
+        int i,x,p=0;
+       for(x=2520;x>0 && p!=1;x+=20)
         {
-
-            for(int i=1;i<=20;i++)
-
+            for(i=11;i<=20;i++)
             {
-
-                if(x%i==0)
-
-                f++;
-
-            }
-
-            if(f==20)
-
-            {
-
-                System.out.println(x);
-                break;
-
-            }
-
-            f=0;
-
-        }
-
-    }
-
+                if(x%i!=0)
+                {
+                    break;
+                }                              
+                else if(i==20)
+                {                                                                                                 
+                    p=1;
+                    break;
+                }               
+            }                        
+       }
+       System.out.println(x=x-20);
+   }
 }
+          
+            
+              
+             
+             
+                 
+                 
+        
+    
+
+        
